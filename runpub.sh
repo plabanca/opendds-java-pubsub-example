e740749ffd48eca3cd4661507561cb482301a435
Phillip LaBanca <labancap@ociweb.com>
2017-09-14 14:13:03 -0500

    Update Env Variables for linux

--- runpub.sh
+++ runpub.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:$ACE_ROOT/lib:$DDS_ROOT/lib:$TEST_ROOT/nexmatix
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ACE_ROOT/lib:$DDS_ROOT/lib:$TEST_ROOT/nexmatix
+export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
 $JAVA_HOME/bin/java -ea -classpath $TEST_ROOT/lib/pubvalve.jar:$TEST_ROOT/lib/nexmatix.jar:$DDS_CLASSPATH PubValve -DCPSBit 0 -DCPSConfigFile rtps_disc.ini -r -w
--- runsub.sh
+++ runsub.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:$ACE_ROOT/lib:$DDS_ROOT/lib:$TEST_ROOT/nexmatix
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ACE_ROOT/lib:$DDS_ROOT/lib:$TEST_ROOT/nexmatix
+export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
 # -r: Set RELIABLE_RELIABILITY_QOS
-# WAIT_FOR_ACKS = -w
+# -w: WAIT_FOR_ACKS
 $JAVA_HOME/bin/java -ea -classpath $TEST_ROOT/lib/subvalve.jar:$TEST_ROOT/lib/nexmatix.jar:$DDS_CLASSPATH SubValve -DCPSBit 0 -DCPSConfigFile rtps_disc.ini -r -w
 
